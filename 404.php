4167f8d00ff30e202d76ad806c722c4ea85cc9c2
natmacira <natimciraolo@gmail.com>
2024-12-03 19:04:27 -0300

    page dkp delete script

--- 404.php
+++ 404.php
@@ -17,7 +17,7 @@ get_header();
 	<p>¿Tu inbox? No tanto. Es hora de igualarlo.</p>
 	<img src="<?php echo esc_url(get_template_directory_uri() . '/img/error-homer.png'); ?>" alt="Imagen de Homero Simpson escondiendose en una pared de arbustos">
 	<p> ¿No sabes por dónde empezar?</p>
-	<a href="">Aquí tres pasos</a>
+	<a href="<?php bloginfo('url'); ?>#divider-take-action">Aquí tres pasos</a>
 </section>
 
 
