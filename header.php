d8d19bf4abf39994529c82db839d4b456855c571
natmacira <natimciraolo@gmail.com>
2024-12-03 14:36:15 -0300

    btn languages esp burger

--- header.php
+++ header.php
@@ -38,7 +38,31 @@
 			</button>
 			<div id="hamburger-menu-container">
 				<?php empty_base_print_menu('hamburger'); ?>
+				<section class="social-media-footer">
+					<div class="languages">
+						<p>Idioma:</p>
+						<details>
+							<summary>
+								<img src="<?php echo esc_url(get_template_directory_uri() . '/img/icono-espanol.png'); ?>" alt="Español">
+								Español
+								<img src="<?php echo esc_url(get_template_directory_uri() . '/img/Icon-arrow.png'); ?>" alt="flecha que indica apertura de un elemento" class="arrow-icon">
+							</summary>
+							<ul>
+								<li><a href="https://deleteryouremails.weareplanta.com/"><img src="<?php echo esc_url(get_template_directory_uri() . '/img/icono-espanol.png'); ?>" alt="English" class="en">English</a></li>
+
+							</ul>
+						</details>
+					</div>
+
+
+					<ul>
+						<li><a href="https://www.facebook.com/people/Borra-Tus-Mails/"><img src="<?php echo esc_url(get_template_directory_uri() . '/img/facebook.png'); ?>" alt="Facebook"></a></li>
+						<li><a href="https://www.instagram.com/borratusmails/"><img src="<?php echo esc_url(get_template_directory_uri() . '/img/instagram.png'); ?>" alt="Instagram"></a></li>
+						<li><a href="https://bsky.app/profile/borratusmails.bsky.social"><img src="<?php echo esc_url(get_template_directory_uri() . '/img/bluesky-logo.png'); ?>" alt="bluesky"></a></li>
+					</ul>
+				</section>
 			</div>
+
 		</div>
 	</header>
 	<main>
