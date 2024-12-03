3fed47abe3d6d8f27527b7d2fe09a5ec2cdfb493
natmacira <natimciraolo@gmail.com>
2024-12-03 19:21:13 -0300

    added all fuentes to content

--- page-fuentes.php
+++ page-fuentes.php
@@ -14,40 +14,53 @@ get_header();
 
 <main>
 
-   <h1> Tenemos la data: noticias y estudios sobre la basura que generan los mails que guardas </h1>
+   <h1> Tenemos la data</h1>
+   <p> noticias y estudios sobre la basura que generan los mails que guardas </p>
 
    <section class="source-inner-container">
       <article class="source-card">
-      <p>fecha</p>
-      <p>Nombre estudio o medio: texto que presenta la data que compartimos. </p>
-      <a href="">Ir a la data</a>
+         <p>21 Mayo 2024.</p>
+         <p>Demandsage. </p>
+         <p>How Many Emails Are Sent Per Day in 2024? </p>
+         <a href="https://www.demandsage.com/how-many-emails-are-sent-per-day/" target="_blank">Ir a la data</a>
       </article>
       <br><br>
       <article class="source-card">
-      <p>fecha</p>
-      <p>Nombre estudio o medio: texto que presenta la data que compartimos. </p>
-      <a href="">Ir a la data</a>
-      </article>
-      <br><br>
-
-      <article class="source-card">
-      <p>fecha</p>
-      <p>Nombre estudio o medio: texto que presenta la data que compartimos. </p>
-      <a href="">Ir a la data</a>
+         <p>2020.</p>
+         <p>Mike Berners-Lee.</p>
+         <p>How Bad Are Bananas? </p>
+         <a href="https://profilebooks.com/wp-content/uploads/wpallimport/files/PDFs/9781788163811_preview.pdf" target="_blank">Ir a la data</a>
       </article>
       <br><br>
 
       <article class="source-card">
-      <p>fecha</p>
-      <p>Nombre estudio o medio: texto que presenta la data que compartimos. </p>
-      <a href="">Ir a la data</a>
+         <p>2019.</p>
+         <p>The Shift Project.</p>
+         <p>Lean ICT</p>
+         <a href="https://theshiftproject.org/en/lean-ict-2/" target="_blank">Ir a la data</a>
       </article>
       <br><br>
 
       <article class="source-card">
-      <p>fecha</p>
-      <p>Nombre estudio o medio: texto que presenta la data que compartimos. </p>
-      <a href="">Ir a la data</a>
+         <p>2024.</p>
+         <p>Basile Fighiera</p>
+         <p>Empreinte carbone d'un e-mail : mythes, réalités et solution. </p>
+         <a href="https://www.sami.eco/blog/empreinte-carbone-email" target="_blank">Ir a la data</a>
+      </article>
+      <br><br>
+
+      <article class="source-card">
+         <p>2019.</p>
+         <p>GreenIT.fr.</p>
+         <p>The Environmental footprint of the digital world. </p>
+         <a href="https://www.greenit.fr/wp-content/uploads/2019/11/GREENIT_EENM_etude_EN_accessible.pdf" target="_blank">Ir a la data</a>
+      </article>
+   
+      <article class="source-card">
+         <p>2022.</p>
+         <p>David Paul. </p>
+         <p>140 Emails Sent Daily Over a Year the Same as Using 16,800 Plastic Bags</p>
+         <a href="https://www.digit.fyi/emails-business-climate-impact-16800-plastic-bags/" target="_blank">Ir a la data</a>
       </article>
    </section>
 </main>
