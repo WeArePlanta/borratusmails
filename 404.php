c71f8c61501d41f2d080de785fb13eec54981bfb
natmacira <natimciraolo@gmail.com>
2024-12-03 18:45:22 -0300

    page error mobile html and styles

--- 404.php
+++ 404.php
@@ -9,7 +9,18 @@
 
 get_header();
 ?>
-	<h2>Lo lamentamos! No encontramos la página que estás buscando.</h2>
+
+<section class="error-section">
+	<p>error</p>
+	<h1>404</h1>
+	<p>Esta página se tomó en serio la limpieza digital y desapareció.</p>
+	<p>¿Tu inbox? No tanto. Es hora de igualarlo.</p>
+	<img src="<?php echo esc_url(get_template_directory_uri() . '/img/error-homer.png'); ?>" alt="Imagen de Homero Simpson escondiendose en una pared de arbustos">
+	<p> ¿No sabes por dónde empezar?</p>
+	<a href="">Aquí tres pasos</a>
+</section>
+
+
 <?php
 
 get_footer();
