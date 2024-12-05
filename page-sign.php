ea13bcab076e3ec7b84903664d52b1ad760054bf
natmacira <natimciraolo@gmail.com>
2024-12-05 19:23:56 -0300

    work in progress styles sign page

--- page-sign.php
+++ page-sign.php
@@ -16,48 +16,56 @@ get_header();
 
 <p class="top-title-pages">Firma la petición</p>
 
-    <section class="section-count-signers">
+<section class="section-count-signers">
+    <article class="article-counter">
         <p>Ya somos más de</p>
         <?php echo do_shortcode('[count_forms]'); ?>
-        <p>personas pidiendo a las empresas que tomen acción para unas bandejas de mail más limpias</p>
-    </section>
-    <section>
-        <p>Somos un movimiento de personas que, como tú, acumula correos en sus bandejas de entrada que fueron útiles en su momento pero que ya no necesita. Estos correos requieren servidores funcionando 24/7, consumiendo energía y recursos. Empresas y personas unidas, podemos reducir este impacto</p>
-    </section>
-    <section class="section-what-how-why">
-
-        <article>
-            <h5>¿Qué queremos?</h5>
-            <p>Que las marcas sean parte del cambio.</p>
-        </article>
-        <article>
-            <h5>¿Cómo?</h5>
-            <p>Adoptando diferentes iniciativas para reducir, medir y compensar este impacto.</p>
-        </article>
-        <article>
-            <h5>¿Por qué?</h5>
-            <p>Porque como usuaries, el almacenamiento de correos a los que nos suscribimos pero que ya no necesitamos contribuye al consumo de energía y emisiones de gases de efecto invernadero. Por ello la basura digital es basura.</p>
-            <p>Si formas parte de una empresa que quiere tomar acción, escríbenos a <a href="mailto:borratusmails@weareplanta.com">borratusmails@weareplanta.com </a> </p>
-        </article>
-
-    </section>
-
-    <section>
-        <p>Súmate para que las empresas formen parte del movimiento. Agrega tu voz para impulsar prácticas digitales más sostenibles y motivar a las empresas a ser parte del cambio.</p>
-    </section>
-
-
-
-    <section class="counter-section">
-        <form id="form-petitorio">
-            <input type="text" id="nombre" name="nombre" placeholder="Tu nombre" required>
-            <input type="email" id="email" name="email" placeholder="Tu email" required>
-            <button type="submit">Firmar Petitorio</button>
-        </form>
-
         <p id="contador-firmas">acá</p>
 
-    </section>
+        <p>personas pidiendo a las empresas que tomen acción para unas bandejas de mail más limpias</p>
+    </article>
+    <article class="article-intro-counter">
+        <p>Somos un movimiento de personas que, como tú, acumula correos en sus bandejas de entrada que fueron útiles en su momento pero que ya no necesita. Estos correos requieren servidores funcionando 24/7, consumiendo energía y recursos. Empresas y personas unidas, podemos reducir este impacto</p>
+    </article>
+</section>
+
+<section class="section-what-how-why">
+
+    <article>
+        <h5>¿Qué queremos?</h5>
+        <p>Que las marcas sean parte del cambio.</p>
+    </article>
+    <article>
+        <h5>¿Cómo?</h5>
+        <p>Adoptando diferentes iniciativas para reducir, medir y compensar este impacto.</p>
+    </article>
+    <article>
+        <h5>¿Por qué?</h5>
+        <p>Porque como usuaries, el almacenamiento de correos a los que nos suscribimos pero que ya no necesitamos contribuye al consumo de energía y emisiones de gases de efecto invernadero. Por ello la basura digital es basura.</p>
+    </article>
+
+</section>
+<section class="mail-to">
+    <p>Si formas parte de una empresa que quiere tomar acción, escríbenos a <a href="mailto:borratusmails@weareplanta.com">borratusmails@weareplanta.com </a> </p>
+</section>
+
+<section class="sumate-section">
+    <p>Súmate </p>
+    <p> para que las empresas formen parte del movimiento.</p>
+    <p> Agrega tu voz para impulsar prácticas digitales más sostenibles y motivar a las empresas a ser parte del cambio.</p>
+</section>
+
+
+
+<section class="form-section">
+    <form id="form-petitorio">
+        <input type="text" id="nombre" name="nombre" placeholder="Tu nombre" required>
+        <input type="email" id="email" name="email" placeholder="Tu email" required>
+        <button type="submit">Firmar Petitorio</button>
+    </form>
+
+
+</section>
 </main>
 
 
